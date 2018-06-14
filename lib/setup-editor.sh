c66664f6cba377cc7b8c77f0b3fa72d502d7aec1
ice-zhao <smallbit@163.com>
2018-06-14 11:37:29 +0800

    setup emacs configuration done

--- lib/setup-editor.sh
+++ lib/setup-editor.sh
@@ -9,8 +9,8 @@
 
 
 main() {
-    Backup_config
-
+    # Backup_config
+    Copy_editor_configuration
 }
 
 
@@ -48,6 +48,7 @@ Backup_config() {
         if [ ! -e dot_emacs.d.tar.xz ]; then
             tar -Jcf dot_emacs.d.tar.xz ${emacs_d} 2>&1 &
             wait
+            rm -rf ${emacs_d} 2>&1
         fi
         popd
         exec 1>&8
@@ -55,9 +56,57 @@ Backup_config() {
     fi
 
     # Log ${emacs_dir}
+    return 0
 }
 
 
+Copy_editor_configuration() {
+    Copy_emacs_config
+    Copy_vim_config
+}
+
+
+Copy_emacs_config() {
+    local cli_emacs_dir="${EDITOR}/emacs"
+    local dot_emacs=${cli_emacs_dir}/dot_emacs
+    local dot_emacs_d=${cli_emacs_dir}/dot_emacs.d
+    local emacs_d_tarball="${cli_emacs_dir}/dot_emacs.d.tar.xz"
+
+    # decompress tarball
+    if [[ -e ${emacs_d_tarball} ]] && [[ ! -e ${dot_emacs_d} ]]
+    then
+        exec 8>&1
+        exec 1>/dev/null
+        pushd ${cli_emacs_dir}
+        tar -Jxf ${emacs_d_tarball##*/} 2>&1 &
+        wait
+        popd
+        exec 8>&1
+        exec 8>&-
+    fi
+
+    # copy to user environment
+    local user_name=`whoami`
+    local home="/home/${user_name}"
+
+    if [ -e ${dot_emacs} ]; then
+        cp -rf ${dot_emacs} ${home}/.emacs
+        wait
+    fi
+
+    if [ -e ${dot_emacs_d} ]; then
+        cp -rf ${dot_emacs_d} ${home}/.emacs.d
+        rm -rf ${dot_emacs_d}
+    fi
+
+    # Log ${cli_emacs_dir}
+    return 0
+}
+
+Copy_vim_config() {
+
+    return 0
+}
 
 
 
