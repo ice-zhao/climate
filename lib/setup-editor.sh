10773cd3e67d2df72b49a22c9c7961aaed746f31
ice-zhao <smallbit@163.com>
2018-06-21 15:32:43 +0800

    backup vim configuration.

--- lib/setup-editor.sh
+++ lib/setup-editor.sh
@@ -2,6 +2,7 @@
 
 . include/local.bh
 . ${LIB}/log
+. ${LIB}/parse
 
 
 
@@ -10,6 +11,7 @@
 
 main() {
     Backup_config
+    Backup_vim_config
     Copy_editor_configuration
 }
 
@@ -55,6 +57,7 @@ Backup_config() {
         exec 8>&-
     fi
 
+    # backup vim configuration
     # Log ${emacs_dir}
     return 0
 }
@@ -105,8 +108,72 @@ Copy_emacs_config() {
 
 Copy_vim_config() {
 
+    install_vim_addons
     return 0
 }
+
+
+install_vim_addons() {
+    declare -a addons=()
+    local vim_addon=`which vim-addons`
+    local options="install"
+
+    addons=(`Parse_src "${EDITOR}/vim/install"`)
+
+    if [ -x ${vim_addon} ]; then
+        for addon in ${addons[*]}; do
+            # ${vim_addon} ${options} ${addon} >/dev/null 2>&1
+            # Log ${addon}
+            return 0
+        done
+    fi
+}
+
+
+Backup_vim_config() {
+    local user_name=`whoami`
+    local backup='backup'
+
+    if [ ! -e ${backup} ]; then
+        mkdir ${backup}
+    fi
+
+    # backup vim config
+    local vim_dir=vim-`date +"%Y-%m-%d"`
+    local bak_vim=${backup}/${vim_dir}
+
+    if [ ! -e  ${bak_vim} ]; then
+        mkdir ${bak_vim}
+    fi
+
+    local user_path="/home/${user_name}"
+    local dot_vimrc="${user_path}"/.vimrc
+    local etc_vim_local="/etc/vim/vimrc.local"
+    if [ -f ${dot_vimrc} ]; then
+        cp -rf ${dot_vimrc} ${bak_vim}/dot_vimrc
+    fi
+
+    if [ -f ${etc_vim_local} ]; then
+        if [ ! -e  ${bak_vim}/etc/vim ]; then
+            mkdir -p ${bak_vim}/etc/vim
+        fi
+        cp -rf ${etc_vim_local} ${bak_vim}/etc/vim/vimrc.local
+    fi
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
